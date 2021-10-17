7a4d09ef1c8dad3e66fbbc228c16260f6a9ee231
Your Name <you@example.com>
2021-10-17 06:35:33 +0200

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 
 const api = {
-  key: "7b68fac815bda2f5ae5e4aff1088f832",
+  key: "5cfd510a1b61652d26a121832f48d41d",
   baseUrl: "https://api.openweathermap.org/data/2.5/weather?q="
 };
 
@@ -15,11 +15,18 @@ function App() {
       try {
         fetch(`${api.baseUrl}${query}&units=metric&appid=${api.key}`)
         .then(response => response.json())
-        .then(response => {          
+        .then(response => {    
+          console.log(response);      
           setLocation(response);
           setQuery("");
         });    
-      } catch(error) {}        
+      } catch(error) {
+        console.log(error);
+        const errorContainer = document.createElement("div");
+        errorContainer.classList.add("b-error-container");
+        errorContainer.innerHTML = "An error occured. Please try again later."
+        document.querySelector("body").appendChild(errorContainer);
+      }        
     }    
   };
 
@@ -27,18 +34,17 @@ function App() {
     <div className={Math.round(location.main.temp) > 18 ? "b-weather-app m-warm" : "b-weather-app m-cold"}>
       <div className="e-app-container">
         <input 
-              type="text"
-              placeholder="Search..."
-              value={query}
-              onChange={event => setQuery(event.target.value)}
-              onKeyPress={getData}
-              className="e-input" 
-          />
-          <h1 className="e-location">{location.name}, {location.sys.country}</h1>
-           <h3 className="e-temperature">{Math.round(location.main.temp)}</h3>
-          <h5 className="e-sky">{location.weather[0].description}</h5>
+          type="text"
+          placeholder="Search..."
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+          onKeyPress={getData}
+          className="e-input" 
+        />
+        <h1 className="e-location">{location.name}, {location.sys.country}</h1>
+        <h3 className="e-temperature">{Math.round(location.main.temp)}</h3>
+        <h5 className="e-sky">{location.weather[0].description}</h5>
       </div>
-      
     </div>
   );
 };
